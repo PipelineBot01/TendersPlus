814e74c7d6b60cefadbf7b9eedc121f9f8d5f425
u7201445 <u7201445@anu.edu.au>
2022-03-25 17:39:28 +1100

    fix<fix bug>

--- ProjectCode/master/Engine/Model/researcher/matching/__init__.py
+++ ProjectCode/master/Engine/Model/researcher/matching/__init__.py
@@ -1,14 +1,1 @@
-import pandas as pd
-from conf.division import RESEARCH_FIELDS
-info=pd.read_csv('../assets/tag_category_map.csv')
-re_dict = {}
-for k, v in RESEARCH_FIELDS.items():
-    re_dict[v['field']] = k
-info.loc[~info['value'].isin(['OTHERS(IRRELEVANT)', 'OTHERS(RELEVANT)']), 'value'] = \
-    info[~info['value'].isin(['OTHERS(IRRELEVANT)', 'OTHERS(RELEVANT)'])]['value'].map(lambda x: re_dict[x])
-print(info['value'].unique())
-# info.to_csv('../assets/new.csv', index=0)
-# info=pd.read_csv('../assets/new.csv')
-# info.loc[~info['value'].isin(['OTHERS(IRRELEVANT)', 'OTHERS(RELEVANT)']), 'value'] = \
-#     info[~info['value'].isin(['OTHERS(IRRELEVANT)', 'OTHERS(RELEVANT)'])]['value'].map(lambda x: RESEARCH_FIELDS[x]['field'])
-# info.to_csv('../assets/new_info.csv', index=0)
+
