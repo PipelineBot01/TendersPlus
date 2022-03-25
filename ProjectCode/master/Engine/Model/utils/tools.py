3be84b5c4edf7b0444e255550b03cee97eb2e1ac
u7201445 <u7201445@anu.edu.au>
2022-03-26 04:36:53 +1100

    docu<add research strength>

--- ProjectCode/master/Engine/Model/utils/tools.py
+++ ProjectCode/master/Engine/Model/utils/tools.py
@@ -5,7 +5,21 @@ from utils.match_utils import normalize
 DIVISION_PATH = '../researcher/assets/' + RESEARCHER_DIVISION_MAP_PATH
 
 
-def get_research_strength(div_path: str = DIVISION_PATH, pk='Staff ID'):
+def get_research_strength(div_path: str = DIVISION_PATH, pk='Staff ID') -> pd.DataFrame:
+    '''
+
+    Parameters
+    ----------
+    div_path: str, path for division data file
+    pk: str, primary key for division dataframe, default as Staff ID.
+
+    This function will generate the research strength for a university, final
+    mark will be compressed to int(3~10)
+
+    Returns
+    -------
+    pd.DataFrame, research strength for a university.
+    '''
     div_df = pd.read_csv(div_path)
     div_df = div_df.groupby('value').agg({pk: 'count',
                                           'weight': 'sum'}).reset_index().sort_values('value').rename(
