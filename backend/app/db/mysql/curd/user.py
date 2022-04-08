086fd6aef5e74d6c2ccc07d5baf0e51495a75fcc
Ryan <ryanyang@lilith.com>
2022-04-09 00:06:47 +0800

    Update user.py

--- backend/app/db/mysql/curd/user.py
+++ backend/app/db/mysql/curd/user.py
@@ -1,8 +1,7 @@
-
 from datetime import datetime
 from sqlalchemy.orm import Session
 
-from typing import Union,List
+from typing import Union, List
 
 from utils import auth
 
@@ -14,13 +13,13 @@ def sql_get_user(email: str, session: Session) -> Union[SQLUser, None]:
     return session.query(SQLUser).filter(SQLUser.email == email).one_or_none()
 
 
-def sql_add_user(email: str, password: str, first_name: str, last_name: str, university: str,n_research_field:int,
+def sql_add_user(email: str, password: str, first_name: str, last_name: str, university: str, n_research_field: int,
                  research_field: list, session: Session) -> SQLUser:
-
     encode_password = auth.encode_password(password)
     first_name_capital = first_name[0].upper() + first_name[1:]
     last_name_capital = last_name[0].upper() + last_name[1:]
-    user_record = SQLUser(email=email, password=encode_password, first_name=first_name_capital, last_name=last_name_capital,
+    user_record = SQLUser(email=email, password=encode_password, first_name=first_name_capital,
+                          last_name=last_name_capital,
                           n_research_field=n_research_field,
                           university=university, create_date=datetime.now(), status=1)
     session.add(user_record)
@@ -30,5 +29,5 @@ def sql_add_user(email: str, password: str, first_name: str, last_name: str, uni
     return user_record
 
 
-def sql_get_all_users(session:Session)->List[SQLUser]:
-    return session.query(SQLUser).all()
+def sql_get_all_users(session: Session) -> List[SQLUser]:
+    return session.query(SQLUser).join(SQLUserResearchField, SQLUser.email == SQLUserResearchField.email).all()
