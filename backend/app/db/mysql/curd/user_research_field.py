be91f2552a32fe63c2787f2dab2e334ea5ebc855
Ryan <ryanyang@lilith.com>
2022-04-09 14:09:01 +0800

    update backend return

--- backend/app/db/mysql/curd/user_research_field.py
+++ backend/app/db/mysql/curd/user_research_field.py
@@ -8,7 +8,13 @@ def sql_get_user_research_field(email: str, n: int, session: Session) -> Union[L
     return session.query(SQLUserResearchField).filter(
         SQLUserResearchField.email == email).limit(n).all()
 
-def sql_add_user_research_field(email:str,field_id:str,session:Session)->SQLUserResearchField:
-    record = SQLUserResearchField(email=email,field_id=field_id)
+
+def sql_add_user_research_field(email: str, field_id: str, session: Session) -> SQLUserResearchField:
+    record = SQLUserResearchField(email=email, field_id=field_id)
     session.add(record)
     return record
+
+
+def sql_get_all_user_research_field(session: Session):
+
+    return [i.__dict__ for i in session.query(SQLUserResearchField).all()]
