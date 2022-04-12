3784a5f70c74c932dcf92c79249fa2e33389a54b
Ryan <ryanyang@lilith.com>
2022-04-12 15:21:21 +0800

    add matcher

--- backend/app/test/test_scheduler.py
+++ backend/app/test/test_scheduler.py
@@ -1,17 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from researcher.update.researcher_updater import ResearcherUpdater
-from tenders.update.tenders_updater import TendersUpdater
 
 bs = BlockingScheduler()
 
-
 def fn():
-    ru = ResearcherUpdater()
-    ru.update()
 
-    tu = TendersUpdater()
-    tu.update()
+    pass
 
 
 bs.add_job(fn, IntervalTrigger(hours=1, timezone='Asia/Hong_Kong'))
