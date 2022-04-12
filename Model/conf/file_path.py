42a6d403608d1f19a0cf2b40fcf603b5723da2f3
u7201445 <u7201445@anu.edu.au>
2022-04-12 15:48:44 +1000

    feat<add feature>

---
+++ Model/conf/file_path.py
@@ -0,0 +1,20 @@
+TENDERS_INFO_PATH = '../tenders/assets/clean_trains_info.csv'
+TENDERS_TAG_MAP_PATH = '../tenders/assets/tenders_tag.csv'
+TENDERS_TOPIC_MAP_PATH = '../tenders/assets/tenders_topic.csv'
+TENDERS_TAG_PATH = '../tenders/assets/tenders_keyword.csv'
+TENDERS_TOPIC_PATH = '../tenders/assets/matching_result_by_lda.csv'
+TENDERS_RELEVANT_PATH = '../tenders/assets/clean_relevant_tenders.csv'
+
+LOCAL_PATH = './assets'
+MODEL_FILE = '../tenders/assets/LdaModel.model'
+
+RESEARCHER_INFO_PATH = '../researcher/assets/researcher_info.csv'
+REG_RESEARCHER_INFO_PATH = '../researcher/assets/reg_researcher_info.csv'
+RESEARCHER_TAG_MAP_PATH = '../researcher/assets/researcher_tag.csv'
+RESEARCHER_DIVISION_MAP_PATH = '../researcher/assets/researcher_division.csv'
+RESEARCHER_TAG_DIV_MAP_PATH = '../researcher/assets/tag_division_map.csv'
+
+TENDERS_MATCHING_OUTPUT = '../output/tenders_match_result.csv'
+RESEARCHER_MATCHING_OUTPUT = '../output/researcher_match_result.csv'
+
+RESEARCHER_MOVEMENT = '../filter/assets/test.csv'
