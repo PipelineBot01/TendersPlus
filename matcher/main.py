59d17ffc6357b631f17c214f8d39c8bd3be6185e
Ryan <ryanyang@lilith.com>
2022-04-13 14:48:59 +0800

    Update main.py

--- matcher/main.py
+++ matcher/main.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 # from researcher.update.researcher_updater import ResearcherUpdater
 # from tenders.update.tenders_updater import TendersUpdater
 # from datetime import datetime, timedelta
@@ -21,7 +22,9 @@ def fn():
 
 
 if __name__ == '__main__':
-    bs = BlockingScheduler(daemon=True)
+    bs = BlockingScheduler(executors={
+        'default':ThreadPoolExecutor(5)
+    },daemon=True)
     bs.add_job(fn, IntervalTrigger(seconds=10, timezone='Asia/Hong_Kong'))
     try:
         bs.start()
