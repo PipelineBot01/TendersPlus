18baf807c2cadc97943745c7176e33aafbe47935
Ryan <ryanyang@lilith.com>
2022-04-13 11:10:27 +0800

    Update main.py

--- matcher/main.py
+++ matcher/main.py
@@ -8,13 +8,16 @@ from datetime import datetime, timedelta
 
 
 def fn():
-    print(f'{datetime.now()} -- start updating')
+    print(f'{datetime.now()} -- start update')
 
     ru = ResearcherUpdater()
     ru.update()
 
     tu = TendersUpdater()
     tu.update()
+    
+    print(f'{datetime.now()} -- done update')
+
 
 bs = BlockingScheduler()
 bs.add_job(fn, IntervalTrigger(hours=1, timezone='Asia/Hong_Kong'),next_run_time=datetime.now()+timedelta(seconds=5))
