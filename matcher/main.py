cf08a3324abe23d260eaada9593406ef2e8bef4d
Ryan <ryanyang@lilith.com>
2022-04-13 12:33:07 +0800

    Update main.py

--- matcher/main.py
+++ matcher/main.py
@@ -1,9 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from researcher.update.researcher_updater import ResearcherUpdater
 from tenders.update.tenders_updater import TendersUpdater
 from datetime import datetime, timedelta
 import requests
+
 
 def fn():
     # print(f'{datetime.now()} -- start update')
@@ -15,13 +16,17 @@ def fn():
     # tu.update()
     #
     # print(f'{datetime.now()} -- done update')
+    print('requesting')
     requests.get('http://110.40.137.110/tendersplus/')
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     fn()
-    bs = BlockingScheduler(daemon=True)
+    bs = BackgroundScheduler(daemon=True)
     bs.add_job(fn, IntervalTrigger(seconds=1, timezone='Asia/Hong_Kong'))
     try:
         bs.start()
+        while True:
+            pass
     except Exception as e:
         bs.shutdown()
