3c7b4e8cb6d12132b0e7b36ffd223b1f2e26d176
Ryan <ryanyang@lilith.com>
2022-04-13 14:41:27 +0800

    Update main.py

--- matcher/main.py
+++ matcher/main.py
@@ -1,21 +1,22 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from researcher.update.researcher_updater import ResearcherUpdater
-from tenders.update.tenders_updater import TendersUpdater
-from datetime import datetime, timedelta
-import requests
+# from researcher.update.researcher_updater import ResearcherUpdater
+# from tenders.update.tenders_updater import TendersUpdater
+# from datetime import datetime, timedelta
+# import requests
 
 
 def fn():
-    print(f'{datetime.now()} -- start update')
-
-    ru = ResearcherUpdater()
-    ru.update()
-
-    tu = TendersUpdater()
-    tu.update()
-
-    print(f'{datetime.now()} -- done update')
+    print('aaa')
+    # print(f'{datetime.now()} -- start update')
+    #
+    # ru = ResearcherUpdater()
+    # ru.update()
+    #
+    # tu = TendersUpdater()
+    # tu.update()
+    #
+    # print(f'{datetime.now()} -- done update')
 
 
 
