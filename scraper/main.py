2e2e4cbaf6ba8983c87e76df3846af2ffa3f5d7f
Ryan <ryanyang@lilith.com>
2022-04-17 21:21:00 +0800

    Update main.py

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,20 @@
-from tenders.update_go import  update_go
+from tenders.update_go import update_go
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from datetime import datetime
+
+
+def fn():
+    if datetime.now().hour < 3:
+        update_go()
 
 
 if __name__ == '__main__':
-    update_go()
+    fn()
+    bs = BlockingScheduler(daemon=True)
+    bs.add_job(fn, 'interval', hours=1)
+    try:
+        bs.start()
+    except Exception as e:
+        bs.shutdown()
