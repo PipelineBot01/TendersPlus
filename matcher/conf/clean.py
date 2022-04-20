0db35e018d98ee3f254bab59ba74ab3f85e485e3
u7201445 <u7201445@anu.edu.au>
2022-04-20 12:03:32 +1000

    refact<tenders update>

---
+++ matcher/conf/clean.py
@@ -0,0 +1,8 @@
+REMAIN_COLS = ['_id', 'Agency', 'Publish Date', 'Close Date & Time', 'Title',
+               'Description',
+               'Eligibility',
+               'Primary Category',
+               'Secondary Category',
+               'Total Amount Available (AUD)',
+               'Location', 'Selection Process', 'GO ID'
+               ]
