d1fa1291f04dabfce7c8d26dd41b7d055a5ba309
u7201445 <u7201445@anu.edu.au>
2022-04-25 12:53:34 +1000

    feat<add matching>

---
+++ matcher/matching.py
@@ -0,0 +1,16 @@
+from filter.filter import Filter
+from researcher.matching.researcher_relation import ResearcherMatcher
+
+rm = ResearcherMatcher()
+fl = Filter()
+
+
+def get_sim_researchers(profile):
+    return rm.match_by_profile(profile)
+
+
+def get_reco_tenders(profile):
+    return fl.match(profile)
+
+if __name__ == '__main__':
+    print(get_reco_tenders({'id': 'Ryanyang@anu.com', 'divisions': ["d_20", "d_21", "d_22"], 'tags': ["Human"]}))
