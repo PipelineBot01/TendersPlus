bb8c1b77feca4e8abf870fca824e1354b0d7e5ee
Ryan <ryanyang@lilith.com>
2022-04-25 09:54:37 +0800

    .

--- matcher/temp.py
+++
@@ -1,3 +0,0 @@
-from utils.tool_utils import get_research_strength
-
-get_research_strength()
--- matcher/utils/tool_utils.py
+++ matcher/utils/tool_utils.py
@@ -1,33 +1,32 @@
 import pandas as pd
+import sys
 from conf.file_path import RESEARCHER_DIVISION_MAP_PATH
 from utils.match_utils import normalize, get_div_id_dict
-from typing import Dict
+from typing import Dict, List
 
-def get_research_strength(div_path: str = RESEARCHER_DIVISION_MAP_PATH, pk='Staff ID') -> Dict[str, dict]:
+
+def get_research_strength(score_range: List[int] = [0, 10],
+                          div_path: str = RESEARCHER_DIVISION_MAP_PATH, pk='id') -> Dict[str, dict]:
     '''
-
     Parameters
     ----------
+    score_range: List[int, int]: range of score
     div_path: str, path for division data file
     pk: str, primary key for division dataframe, default as Staff ID.
-
     This function will generate the research strength for a university, final
     mark will be compressed to int(3~10)
-
     Returns
     -------
     dict[str, dict]
     '''
 
     div_df = pd.read_csv(div_path)
-    print(div_df.head(),div_df.columns)
-    div_df = div_df.groupby('value').agg({pk: 'count',
-                                          'weight': 'sum'}).reset_index().sort_values('value').rename(
+    div_df = div_df.groupby('division').agg({pk: 'count',
+                                             'weight': 'sum'}).reset_index().sort_values('division').rename(
         columns={pk: 'count', 'weight': 'Total_weight'})
-    div_df = div_df[~div_df['value'].isin(['OTHERS(RELEVANT)', 'OTHERS(IRRELEVANT)'])]
-    div_df = normalize(div_df, 'Total_weight', method='rank')[['value', 'Total_weight']].rename(
-        columns={'value': 'research_field', 'Total_weight': 'score'})
-
+    div_df = div_df[~div_df['division'].isin(['OTHERS(RELEVANT)', 'OTHERS(IRRELEVANT)'])]
+    div_df = normalize(div_df, 'Total_weight', method='rank', bounds=score_range)[['division', 'Total_weight']].rename(
+        columns={'division': 'research_field', 'Total_weight': 'score'})
     div_id_dict = get_div_id_dict()
     div_df['research_field'] = div_df['research_field'].map(lambda x: div_id_dict[x])
 
