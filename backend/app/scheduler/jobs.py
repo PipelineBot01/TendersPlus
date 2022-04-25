db546c6f3faf677f80f15281ac84808acfb50986
Ryan <ryanyang@lilith.com>
2022-04-25 16:51:31 +0800

    Update jobs.py

--- backend/app/scheduler/jobs.py
+++ backend/app/scheduler/jobs.py
@@ -31,14 +31,17 @@ async def get_all_user_info():
             df_all_user_research_field = pd.DataFrame.from_records(sql_get_all_user_research_field(db)).groupby('email',
                                                                                                                 as_index=False).agg(
                 {'field_id': lambda x: list(x)})
+            df_all_user_research_field.rename(columns={'field_id': 'divisions'}, inplace=True)
+
             df_all_user_tag = pd.DataFrame.from_records(sql_get_all_user_tag(db)).groupby('email', as_index=False).agg(
                 {'name': lambda x: list(x)})
+            df_all_user_tag.rename(columns={'name':'tags'},inplace=True)
+
             data = pd.merge(df_all_user_research_field, df_all_user_tag, how='left', on='email')
             data = pd.merge(df_all_user, data, how='inner', on='email')
             data.fillna('',inplace=True)
-            data.rename(columns={'field_id':'divisions','name':'tags'},inplace=True)
+
             settings.USER_INFO = data.to_dict('records')
-            print(settings.USER_INFO)
 
 
 @job(id='get_all_user_action', trigger=IntervalTrigger(hours=1, timezone='Asia/Hong_Kong'), delay=False)
