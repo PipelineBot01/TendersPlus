6223712835a960ce62d45a2779d2ed86a732d2f1
u7201445 <u7201445@anu.edu.au>
2022-04-29 17:35:49 +1000

    fix

--- matcher/matching.py
+++ matcher/matching.py
@@ -11,7 +11,7 @@ def get_reco_tenders(profile):
     return fl.match(profile)
 
 if __name__ == '__main__':
-    # profile = {"id": "Test@0.com","divisions": ["d_08", "d_10", "d_09"], "tags": []}
-    # tmp_df = get_reco_tenders(profile)
-    # print(reco_process.run(profile['id'], tmp_df))
-    print(get_sim_researchers({"id": "Ryanyang@anu.com", "divisions": ["d_20", "d_21", "d_22"], "tags": ["Human"]}))
+    profile = {"id": "Test@0.com","divisions": ["d_08", "d_10"], "tags": []}
+    tmp_df = get_reco_tenders(profile)
+    print(tmp_df)
+    # print(get_sim_researchers({"id": "Ryanyang@anu.com", "divisions": ["d_20", "d_21", "d_22"], "tags": ["Human"]}))
