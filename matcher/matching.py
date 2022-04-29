4dac7a2359d4177b9ef2137a5ee26f2442c6eebb
u7201445 <u7201445@anu.edu.au>
2022-04-29 12:33:10 +1000

    fix

--- matcher/matching.py
+++ matcher/matching.py
@@ -1,11 +1,11 @@
 from auto_reco.clb_filter import Filter
-# from researcher.matching.researcher_relation import ResearcherMatcher
+from researcher.matching.researcher_relation import ResearcherMatcher
 from auto_reco import reco_process
-# rm = ResearcherMatcher()
+rm = ResearcherMatcher()
 fl = Filter()
 
-# def get_sim_researchers(profile):
-#     return rm.match_by_profile(profile)
+def get_sim_researchers(profile):
+    return rm.match_by_profile(profile)
 
 def get_reco_tenders(profile):
     return fl.match(profile)
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     # profile = {"id": "Test@0.com","divisions": ["d_08", "d_10", "d_09"], "tags": []}
     # tmp_df = get_reco_tenders(profile)
     # print(reco_process.run(profile['id'], tmp_df))
-    print(reco_process.get_hot_tenders())
+    print(get_sim_researchers({"id": "Ryanyang@anu.com", "divisions": ["d_20", "d_21", "d_22"], "tags": ["Human"]}))
