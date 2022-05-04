1043df024f3277f25a40e4f16c26703d6c8d9472
Ryan <ryanyang@lilith.com>
2022-05-04 12:41:24 +0800

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -47,7 +47,7 @@ class Settings(BaseSettings):
     # RECORD ALL USER
     USER_INFO = []
     USER_ACTION = []
-    USER_INFO_DF: dict = None
+    USER_INFO_DF: dict = {}
     ADMIN_USER_TOKEN: str
 
     UNIVERSITIES = ['Australian National University', 'University of Canberra', 'Australian Catholic University',
